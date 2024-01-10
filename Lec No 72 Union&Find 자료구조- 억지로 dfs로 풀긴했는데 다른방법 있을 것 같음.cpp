613a4205764501b835cfee3335a2fabc0a32385f
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-01-10 17:19:18 +0900

    Update Lec No 72 Union&Find 자료구조- 억지로 dfs로 풀긴했는데 다른방법 있을 것 같음.cpp

--- Lec No 72 Union&Find 자료구조- 억지로 dfs로 풀긴했는데 다른방법 있을 것 같음.cpp
+++ Lec No 72 Union&Find 자료구조- 억지로 dfs로 풀긴했는데 다른방법 있을 것 같음.cpp
@@ -2,65 +2,62 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+vector<int>unf(1001,0);
 
-void dfs(int);
-vector<pair<int,int>>f;
-vector<int>f_res;
-int ch[1000];
-int n,m;
-int result;
-bool flag = 0;
+int find(int v){
+    if(v == unf[v]){
+        return v;
+    }else{
+        return unf[v] = find(unf[v]); // 이게 경로 축약해주는거 [중간단계 단축] + 재귀함수
+    }
+
+}
+
+void Union(int a, int b){
+    int x = find(a);
+    int y = find(b);
+    if(x!=y){
+        unf[x]=y;
+    }
+
+}
+//Union find 결과적으로 서로소 집합 2개가 생기는거 
+//-> 새로운 input data가 여러집단중 하나에만 속하게 되는 경우 이경우에 배열사용 트리형태로 구현한다 
+//-> 실제로 그렇진않지만 같은 집합이면 어느 요소나 타고 올라가면 같은 루트가 나온다고 생각하면됨 ( 제일 마지막 unf[v]==v 인 애가 root가 되는거임)
+//-> 그니까 결국 차례로 연결하고 맨 끝 루트를 통해 어떤 집합인지 확인하는거.
+
+// 처음에는 unf[i] = i 로 초기화 해줌
+// 1 2 3 4 5 6 7 8 9 node
+// 1 2 3 4 5 6 7 8 9 connection to
+//여기서 부터 input 받아서 find랑 union함수 실행
+//한번씩 손코딩하면서 왔다갔다 해보는게 중요 .. 마지막에 2개 들어왔을때 find(a) == find(b) ?? 하면 둘이 같은 집합인지 확인할 수 있음 find가 재귀함수로 root까지 갈테니까
 
 int main(){
-
     freopen("input.txt","rt",stdin);
+    int n , m;
+    cin >> n >> m;
 
-    cin >> n>>m;
-    
-    //양방향이네... ?...?
-    for(int i = 0; i<m;i++){
+    for(int i = 1; i<=n;i++){
+        unf[i] = i;    
+    }
+    for(int i = 1; i<=m;i++){
         int a,b;
         cin >> a>>b;
-        f.push_back(make_pair(a,b));
+        Union(a,b);
     }
-    int x;
-    cin >> x >> result;
-    
-    dfs(x);
-    for(int i = 0 ; i<f_res.size();i++){
-       if(f_res[i] == result){
-            flag = 1;
-       }
-    }
-    if(flag){
-        cout << "YES";
+    int a,b;
+    cin >> a>>b;
+    if(find(a)==find(b)){
+        cout<<"YES";
     }else{
         cout<<"No";
-    }
+    } 
+    
+
+
     return 0;
 }
-void dfs(int x){
-    
-    if(ch[x]==1){
-        return;
-    }
-    cout<< " x: "<<x<<" \n ";
-    for(int i = 0 ; i<f.size();i++){
-        
-        if(f[i].first == x ){
-            //cout << " first : " << f[i].first << " second: " << f[i].second << " i: "<<i<<" x: "<<x<<" \n ";
-            f_res.push_back(f[i].second);
-            ch[x] = 1;
-            dfs(f[i].second);
-        }else if(f[i].second == x){
-            //cout << " first : " << f[i].first << " second: " << f[i].second << " i: "<<i<<" x: "<<x<<" \n ";
-            f_res.push_back(f[i].first);
-            ch[x] = 1;
-            dfs(f[i].first);
-        }else{
 
-        }
-    }
+// 1 2 3 4 5 6 7 8 9
+// 1 2 3 4 5 6 7 8
 
-
-}
