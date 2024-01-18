cc842fc69c380da4a7a1340f4ecf195df3eebd23
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-01-18 09:29:32 +0900

    .

--- Lec No 81  DFS 활용 삼성 문제풀이 -3  복잡하고 오래걸렸지만 잘 풀었음 여러문제 조합된 느낌.cpp
+++ Lec No 81  DFS 활용 삼성 문제풀이 -3  복잡하고 오래걸렸지만 잘 풀었음 여러문제 조합된 느낌.cpp
@@ -12,8 +12,9 @@ vector<pair<int,int>>cd;
 int n,m;
 int sr = 0;
 int lv = 0;
-
-//n-m = 없앨 피자집 갯수
+int ch[50];
+int res_min = 2147000000;
+// lv = 없앨 피자집 갯수
 // second = x= j // first = y = i
 
 //cal_dist = x,y 좌표에 들어간 1기준으로 최소 피자 배달거리 return.
@@ -34,24 +35,35 @@ int cal_dist(int x,int y,vector<vector<int>>temp){
 }
 
 void dfs(int v){
+    // v를 기준으로 2를 하나씩 지워가면서 모든 지워진 2의 조합에서 cal_dist를 때리는거임.
     if(v>lv){
-        return;
-    }else{
-        vector<vector<int>>temp(13,vector<int>(13,0));
+        int Min = 2147000000;
+        int sum = 0;
         for(int i = 1;i<=n;i++){
-            for(int j = 1; j<=n;j++){
-                temp[i][j] = map[i][j];
-            }
-        }// 여기서 cd에 있는애들 dfs로 순차적으로 0으로 만들어줘야할듯
-        
-        // 최소거리 계산 , map or temp가지고 dist채워야함.
-        for(int i = 1; i<=n ; i++){
-            for(int j = 1; j<=n;j++){
-                if(temp[i][j]==1){
-                    int res = cal_dist(j,i,temp);
+            for(int j = 1;j<=n;j++){
+                if(map[i][j]==1){
+                    sum = sum + cal_dist(j,i,map);
+                    
                 }
             }
-        }   
+        }
+        if(res_min>sum){
+            res_min = sum;
+        }
+        
+        return;
+    }else{
+        
+        for(int i =0;i<cd.size();i++){
+            if(ch[i]==0){
+                map[cd[i].first][cd[i].second] = 0;     // 여기서 2지워주고
+                ch[i] = 1;
+                dfs(v+1);
+                map[cd[i].first][cd[i].second] = 2;     //dfs끝나면 다시 2넣어주고
+                ch[i] = 0;
+            }
+        }
+        
 
     }    
  
@@ -69,12 +81,13 @@ int main(){
             if(temp == 2){
                 sr++;
                 cd.push_back(make_pair(i,j));
+                //cd = 2인애들 좌표 저장
             } // 피자집 갯수
             map[i][j] = temp;
         }
     }
     lv = sr - m;
-    //dfs(1);
-
+    dfs(1);
+    cout << res_min;
     return 0;
 }
