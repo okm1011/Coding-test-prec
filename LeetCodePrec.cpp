777d3631d95ca9366b177258892f219e21a19060
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-07-02 17:31:25 +0900

    Prob 26

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,37 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-         // second method -> since it is already sorted , i can use two pointer
-         vector<int> answer;
-         int num1_ptr = 0 , num2_ptr = 0;
-         while(num1_ptr < m || num2_ptr < n ){
-            if(num1_ptr >= m && num2_ptr < n){
-                answer.push_back(nums2[num2_ptr]);
-                num2_ptr++;     
-                continue;           
-            }else if(num2_ptr >= n && num1_ptr < m){
-                answer.push_back(nums1[num1_ptr]);
-                num1_ptr++;     
-                continue;      
+    int removeDuplicates(vector<int>& nums) {
+        int k = nums.size();
+        vector<int> answer;
+        answer.push_back(nums[0]);
+        for(int i =1 ; i < nums.size() ; i++){
+            if(nums[i] == answer[answer.size()-1]){
+                k--;
             }else{
-                if(nums1[num1_ptr]>nums2[num2_ptr]){
-                    answer.push_back(nums2[num2_ptr]);
-                    num2_ptr++;
-                }else if(nums1[num1_ptr]<nums2[num2_ptr]){
-                    answer.push_back(nums1[num1_ptr]);
-                    num1_ptr++;
-                }else{
-                    answer.push_back(nums1[num1_ptr]);
-                    answer.push_back(nums1[num1_ptr]);
-                    num1_ptr++;
-                    num2_ptr++;
-                }
+                answer.push_back(nums[i]);
             }
-
-         }
-         nums1 = answer;
+        }        
+        nums = answer;
+        return k;
     }
 };
