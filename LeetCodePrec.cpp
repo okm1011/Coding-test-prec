37ccc0c8ba1c662152b910fd3e4bde1fba1d7120
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-07-02 16:54:23 +0900

    88 Prob

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int k = nums.size();
-        vector<int> answer;
-        for(int i=0; i<nums.size();i++){
-            if(nums[i] == val){
-                k--;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+         // second method -> since it is already sorted , i can use two pointer
+         vector<int> answer;
+         int num1_ptr = 0 , num2_ptr = 0;
+         while(num1_ptr < m || num2_ptr < n ){
+            if(num1_ptr >= m && num2_ptr < n){
+                answer.push_back(nums2[num2_ptr]);
+                num2_ptr++;     
+                continue;           
+            }else if(num2_ptr >= n && num1_ptr < m){
+                answer.push_back(nums1[num1_ptr]);
+                num1_ptr++;     
+                continue;      
             }else{
-                answer.push_back(nums[i]);
+                if(nums1[num1_ptr]>nums2[num2_ptr]){
+                    answer.push_back(nums2[num2_ptr]);
+                    num2_ptr++;
+                }else if(nums1[num1_ptr]<nums2[num2_ptr]){
+                    answer.push_back(nums1[num1_ptr]);
+                    num1_ptr++;
+                }else{
+                    answer.push_back(nums1[num1_ptr]);
+                    answer.push_back(nums1[num1_ptr]);
+                    num1_ptr++;
+                    num2_ptr++;
+                }
             }
-        }
-        sort(answer.begin(),answer.end());
-        nums = answer;
-        return k;
+
+         }
+         nums1 = answer;
     }
 };
