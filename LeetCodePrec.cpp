ffa69e5c2a510e7b2abe8f1435ecc2f37d87670f
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-07-03 20:52:35 +0900

    prob 80

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
+#include <map>
 
+using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int k = nums.size();
         vector<int> answer;
         answer.push_back(nums[0]);
-        for(int i =1 ; i < nums.size() ; i++){
+        int k = nums.size();
+        int cnt = 0;
+        for(int i = 1 ; i < nums.size();i++){
             if(nums[i] == answer[answer.size()-1]){
-                k--;
+                if(cnt == 0){
+                    answer.push_back(nums[i]);
+                    cnt++;
+                }else if(cnt == 1){
+   
+                    k--;
+                }
             }else{
                 answer.push_back(nums[i]);
+                cnt = 0;
             }
-        }        
+        }
         nums = answer;
         return k;
+
     }
 };
