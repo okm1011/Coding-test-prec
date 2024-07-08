0251a848602a1bd4e82d90fece13a5cc84727aba
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-07-08 18:39:54 +0900

    prob 274

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -2,40 +2,27 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int Min;
-
 
 class Solution {
 public:
-    void dfs(vector<int>& num,int start , int step_left ,int cnt ){
-        if(cnt >= Min)return;
-        for(int i = 1 ; i<=step_left ; i++){
-            if(start+i >= num.size()-1){
-                if(cnt+1<Min)Min=cnt+1;
-                return;
-            }else{
-                dfs(num, start + i , max(step_left - i , num[start + i]) ,cnt + 1 );
+    int hIndex(vector<int>& citations) {
+        int h_index = 1;
+        while(1){
+            int cnt = 0;
+            bool index_done = false;
+            for(int i = 0 ; i<citations.size();i++){
+                if(citations[i]>=h_index){
+                    cnt ++;
+                    if(cnt >= h_index){
+                        h_index++;
+                        index_done = true;
+                        break;
+                    }
+                }
             }
-            
+            if(!index_done)break;
+
         }
-    }
-    int jump(vector<int>& nums) {
-        int answer = 0;
-        // this should be dfs..
-        Min = 100001; // Reset Min for each test case
-        if(nums.size() <= 1) return 0;
-        dfs(nums,0,nums[0],0);
-        return Min;
+        return h_index-1;
     }
 };
-
-
-
-int main(){
-    vector<int>num = {2,1};
-    Solution s;
-    int answer = s.jump(num);
-    cout << answer;
-    
-    
-}
