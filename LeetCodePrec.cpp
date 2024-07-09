196a53e2928519270644f5849b49152d977d3433
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-07-09 19:19:31 +0900

    prob 380

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
+#include <map>
+
 using namespace std;
 
-class Solution {
+class RandomizedSet {
 public:
-    int hIndex(vector<int>& citations) {
-        int h_index = 1;
-        while(1){
-            int cnt = 0;
-            bool index_done = false;
-            for(int i = 0 ; i<citations.size();i++){
-                if(citations[i]>=h_index){
-                    cnt ++;
-                    if(cnt >= h_index){
-                        h_index++;
-                        index_done = true;
-                        break;
-                    }
-                }
-            }
-            if(!index_done)break;
+    map<int,int>m;
+    RandomizedSet() {
 
+    }
+    
+    bool insert(int val) {
+        if(m.find(val) == m.end()){
+            m.insert(make_pair(val,1));
+            return true;
+        }else{
+            return false;
         }
-        return h_index-1;
+    }
+    
+    bool remove(int val) {
+        if(m.find(val) != m.end()){
+            m.erase(m.find(val));
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    int getRandom() {
+        random_device rd;
+        mt19937 mt(rd());
+        uniform_int_distribution<int> dist(0,m.size()-1);
+        auto randNum = dist(mt);
+        map<int,int>::iterator iter = m.begin();
+        for(int i = 0 ; i<randNum ;i++){
+            iter++;
+        }
+        return iter->first; 
     }
 };
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+
