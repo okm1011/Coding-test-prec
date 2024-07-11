3e23104fa6425d0cb2dde5ebb60eb9d99c7dfe48
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-07-11 20:42:54 +0900

    135

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,36 +1,21 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>answer(n,0);
-        vector<int>left(n,0);
-        vector<int>right(n,0);
-        left[0] = nums[0];
-        right[n-1] = nums[n-1];
-
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int>left(n, 1), right(n, 1);
         
-
-        for(int i = 1; i<n;i++){
-            left[i] = left[i-1] * nums[i];
-        }
-        for(int i = n-2 ; i>=0;i--){
-            right[i] = right[i+1] * nums[i];
-        }
-        for(int i = 0 ; i<n ; i++){
-            if(i == 0){
-                answer[i] = right[i+1];
-            }else if(i == n-1){
-                answer[i] = left[i-1];
-            }else{
-                answer[i] = left[i-1] * right[i+1];
-            }
-            
-        }
-        return answer;
-
+        //traverse left to right and compare curr value with left side value
+        for(int i = 1; i < n; i++)
+            if(ratings[i] > ratings[i-1]) left[i] = left[i-1]+1;
+              
+         //traverse right to left and compare curr value with right side value
+        for(int i = n-2; i >= 0; i--)
+            if(ratings[i] > ratings[i+1]) right[i] = right[i+1]+1;
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+            ans += max(left[i], right[i]);
+        
+        return ans;            
     }
 };
