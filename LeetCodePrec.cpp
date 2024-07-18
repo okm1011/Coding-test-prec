2ca50570946c45bbb14f16b7cb602ad149553095
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-07-18 19:11:36 +0900

    prob 12

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,40 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <string>
 using namespace std;
-
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        int l_max = height[0];
-        int r_max = height[n-1];
-        int l_pos = 1 , r_pos = n-2;
-        int water_cnt = 0;
-        while(l_pos<=r_pos){
-            if(height[l_pos]>=l_max){
-                l_max = height[l_pos];
-                l_pos++;
-            }else if(height[r_pos] >= r_max){
-                r_max = height[r_pos];
-                r_pos--;                
-            }else if(l_max <= r_max ){
-                water_cnt += l_max - height[l_pos];
-                l_pos++;
-            }else{
-                water_cnt += r_max - height[r_pos];
-                r_pos--;
-            }
+    string intToRoman(int num) {
+        vector<int>nums;
+        vector<string> answer_tmp;
+        map<int,vector<string>>m;
+        
+        vector<string>one = {"I","X","C","M"};
+        vector<string>five = {"V","L","D"};
+        vector<string>four = {"IV","XL","CD"};
+        vector<string>nine = {"IX","XC","CM"};
+        m.insert(make_pair(1,one));
+        m.insert(make_pair(5,five));
+        m.insert(make_pair(4,four));
+        m.insert(make_pair(9,nine));
+        while(num != 0){
+            //cout << num%10 << "\n";
+            nums.push_back(num%10);
+            num /= 10;
         }
 
-        return water_cnt;
+        for(int i = 0; i<nums.size();i++){
+            if(nums[i] == 1 || nums[i] == 4 || nums[i] == 5 || nums[i] == 9){
+                answer_tmp.push_back(m[nums[i]][i]);
+            }else{
+                if(nums[i]>5){
+                    for(int j = 0 ; j<nums[i]-5 ; j++){
+                        answer_tmp.push_back(m[1][i]);
+                    }
+                    answer_tmp.push_back(m[5][i]);
+                }else{
+                    if(nums[i]==0)continue;
+                    for(int j = 0 ; j<nums[i] ; j++){
+                        answer_tmp.push_back(m[1][i]);
+                    }
+                }
+            }
+            
+        }
+        string answer = "";
+        for(int i = answer_tmp.size()-1; i>=0 ; i--){
+            answer += answer_tmp[i];
+        }
+        return answer;
+
     }
 };
 
 int main(){
-    Solution S;
-    vector<int> input = {4,2,0,3,2,5};
-    int answer = S.trap(input);
+    Solution s;
+    string answer = s.intToRoman(1994);
+    cout <<answer;
 
     return 0;
 }
