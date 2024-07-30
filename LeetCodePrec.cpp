5cb1056e0ba1306de1274bde9efcd84fd8ccab22
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-07-30 20:25:38 +0900

    napsack

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -3,18 +3,35 @@
 #include <algorithm>
 using namespace std;
 
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());      
-        nums.erase(unique(nums.begin(),nums.end()),nums.end());  
-    }
-};
+
 
 int main(){
-    vector<int>nums = {-1,0,1,2,-1,-4};
-    Solution s;
-    vector<vector<int>>anwer = s.threeSum(nums);
+    freopen("input.txt","rt",stdin);
+    int n , k;
+    cin >> n >> k;
+    vector<pair<int,int>>data;
+    for(int i = 0 ; i < n ; i++){
+        int weight , value;
+        cin >> weight >>value;
+        data.push_back(make_pair(weight,value));
+    }
+    vector<vector<int>>dp(k+1,vector<int>(n,0));
+    //dp니까 첫줄은 채워줘야함
+    for(int i = 1 ; i<=k ; i++){
+        if(i>=data[0].first){
+            dp[i][0] = data[0].second;
+        }
+    }
+    for(int j = 1 ; j<n ; j++){
+        for(int i = 1 ; i <= k ; i++){
+            if(i >= data[j].first){
+                dp[i][j] = max(dp[i][j-1] , dp[i-data[j].first][j-1]+data[j].second);
+            }else{
+                dp[i][j] = dp[i][j-1];
+            }
+        }
+    }
+    cout << dp[k][n-1];
 
     return 0;
 }
