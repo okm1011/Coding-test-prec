48bd1bb8fffa7828bc9d92338c790f9296b5a43a
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-03 22:28:54 +0900

    평범한 배낭2

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -7,24 +7,31 @@ using namespace std;
 int main(){
 
     freopen("input.txt","rt",stdin);
-    int n,k;
-    cin>>n>>k;
-    vector<pair<int,int>>bag;
-    for(int i = 0 ; i<n;i++){
-        int w,v;
-        cin >> w >> v;
-        bag.push_back(make_pair(w,v));
-    }
-    vector<int>dp(100001,0);
-    //똑같아 가로 물건이고 세로 맥스무게인데 어차피 끝에줄만 쓸거니까 세로 한줄에 계속 덮어씌우는느낌.
-    for(int i = 0 ; i<n;i++){
-        for(int j = 1 ; j<=k;j++){
-            if(j>=bag[i].first){
-                dp[j] = max(dp[j],dp[j-bag[i].first]+bag[i].second);
+    int n,m;
+    cin >>n >>m;
+    // n = 물건 종류 , m = max 무게
+    vector<pair<int,int>>data;
+    for(int i = 0 ; i<n ;i++){
+        int weight , value , q;
+        cin >> weight >> value >> q;
+        int sum = 0;
+        for(int j = 1 ; j<=q ; j*=2){
+            sum+=j;
+            if(sum > m){
+                break;
+            }else{
+                data.push_back(make_pair(weight*j,value*j));
             }
         }
     }
-    cout<<dp[k];
+    // 이러면 원래 배낭문제로 풀수 있음 여러개있는거를 걍 묶음 상품으로 바꿔버리는거지. 근데 이걸 2의 배수로 바꾸면 모든 경우의 수의 조합이 나온다는거임.
+    vector<int>dp(m+1,0);
+    for(int i = 0 ; i<data.size();i++){
+        for(int j = m;j>=data[i].first;j--){
+            dp[j] = max(dp[j],dp[j-data[i].first]+data[i].second);
+        }
+    }
+    cout << dp[m];
 
     return 0;
 }
