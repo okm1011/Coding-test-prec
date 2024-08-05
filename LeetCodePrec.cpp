8153f709461d95a0c3365b3fd269265468a8fc4f
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-05 20:49:01 +0900

    15. 3Sum

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -3,35 +3,19 @@
 #include <algorithm>
 using namespace std;
 
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>answer;
+        // 정렬해놓고 
+        sort(nums.begin(),nums.end());
+        return answer;
+    }
+};
 
 int main(){
 
-    freopen("input.txt","rt",stdin);
-    int n,m;
-    cin >>n >>m;
-    // n = 물건 종류 , m = max 무게
-    vector<pair<int,int>>data;
-    for(int i = 0 ; i<n ;i++){
-        int weight , value , q;
-        cin >> weight >> value >> q;
-        int sum = 0;
-        for(int j = 1 ; j<=q ; j*=2){
-            sum+=j;
-            if(sum > m){
-                break;
-            }else{
-                data.push_back(make_pair(weight*j,value*j));
-            }
-        }
-    }
-    // 이러면 원래 배낭문제로 풀수 있음 여러개있는거를 걍 묶음 상품으로 바꿔버리는거지. 근데 이걸 2의 배수로 바꾸면 모든 경우의 수의 조합이 나온다는거임.
-    vector<int>dp(m+1,0);
-    for(int i = 0 ; i<data.size();i++){
-        for(int j = m;j>=data[i].first;j--){
-            dp[j] = max(dp[j],dp[j-data[i].first]+data[i].second);
-        }
-    }
-    cout << dp[m];
-
-    return 0;
+    vector<int>input = {-1,0,1,2,-1,-4};
+    Solution s;
+    vector<vector<int>>answer = s.threeSum(input);
 }
