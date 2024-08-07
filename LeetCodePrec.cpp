c7c1c17c5b204667f906ce966b9821fe8fa00a79
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-07 21:21:47 +0900

    30. Substring with Concatenation of All Words

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
-#include <map>
 using namespace std;
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int>ch(200,0);
-        int answer = 0;
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int>ch(words.size(),0);
+        int length = words[0].size();
+        vector<int>answer;
         int left = 0;
-        for(int right = 0; right<s.size();right++){
-            ch[s[right]]++;
-            if(ch[s[right]]==1){
-                answer = max(answer,right-left+1);
-            }else{
-                while(s[left]!=s[right] && left<=right){
-                    ch[s[left]]--;
-                    left++;                    
+        int cnt = 0;
+        //left가 answer에 들어가는 시작지점이 되는거임.
+        for(int right = 0 ; right<s.size();right+=length){
+            string temp = s.substr(right,length);
+            cout << "ch: " << find(words.begin(),words.end(),temp)- words.begin() << "\n";
+            if(find(words.begin(),words.end(),temp) == words.end()){
+                left=right+length;
+                fill_n(ch.begin(),ch.size(),0);
+                cnt = 0;
+            }else if(ch[find(words.begin(),words.end(),temp) - words.begin()] == 0){
+                ch[find(words.begin(),words.end(),temp)- words.begin()] = 1;
+                cnt++;
+                if(cnt == words.size()){
+                    answer.push_back(left);
+                    ch[find(words.begin(),words.end(),s.substr(left,length))- words.begin()] = 0;
+                    cnt--;
+                    left+=length;
                 }
-                ch[s[left]]--;
-                left++;  
-                answer = max(answer,right-left+1);    
+
+            }else{
+                left = right;
+                fill_n(ch.begin(),ch.size(),0);
+                cnt = 1;
+                ch[find(words.begin(),words.end(),temp) - words.begin()] = 1;
             }
         }
         return answer;
     }
 };
 int main(){
-    string input = "abcabcbb";
+    string input = "barfoothefoobarman";
+    vector<string>input2 = {"foo","bar"};
     Solution s;
-    int answer = s.lengthOfLongestSubstring(input);
-
+    vector<int>answer = s.findSubstring(input , input2);
     return 0;
 }
