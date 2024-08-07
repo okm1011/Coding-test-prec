ea9b2f14f8940616a3e2e924c8e90f416f9725f1
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-07 19:30:21 +0900

    3. Longest Substring Without Repeating Characters

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,26 +1,35 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<queue>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
 using namespace std;
-
 class Solution {
 public:
-    int answer = INT_MAX;
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int sum = 0;
+    int lengthOfLongestSubstring(string s) {
+        vector<int>ch(200,0);
+        int answer = 0;
         int left = 0;
-        for(int right = 0 ; right<nums.size() ; right++){
-            sum+=nums[right];
-            if(sum >= target){             
-                while(sum>=target){
-                    if(answer > right-left + 1)answer = right-left + 1;
-                    sum-=nums[left];
-                    left++;
+        for(int right = 0; right<s.size();right++){
+            ch[s[right]]++;
+            if(ch[s[right]]==1){
+                answer = max(answer,right-left+1);
+            }else{
+                while(s[left]!=s[right] && left<=right){
+                    ch[s[left]]--;
+                    left++;                    
                 }
+                ch[s[left]]--;
+                left++;  
+                answer = max(answer,right-left+1);    
             }
         }
-        if(answer == INT_MAX)return 0;
         return answer;
     }
 };
+int main(){
+    string input = "abcabcbb";
+    Solution s;
+    int answer = s.lengthOfLongestSubstring(input);
+
+    return 0;
+}
