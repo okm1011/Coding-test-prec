c8880b81202c95386fa486779d106ff7c549eac5
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-08 22:30:32 +0900

    54. Spiral Matrix- 레전드 문제

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        //이거 아는거임
-        int dx[] = {1,0,-1,0};
-        int dy[] = {0,1,0,-1};
-        int n = matrix.size();
-        int m = matrix[0].size() ;
-        vector<vector<int>> ch(n,vector<int>(m,0));
-        int cnt_max = n*m;
-        int direc = 0;
-        int x = 0;
-        int y = 0;
-        vector<int>answer;
-        while(answer.size() < cnt_max){
-            answer.push_back(matrix[y][x]);
-            ch[y][x] = 1;
-            int n_x = x+dx[direc];
-            int n_y = y+dy[direc];
-            if(n_x < m && 0<=n_x && n_y <n && 0<=n_y && ch[n_y][n_x]==0){
-                x = n_x;
-                y = n_y;
-            }else{
-                direc++;
-                if(direc>3){
-                    direc = 0;
-                }
-                x = x+dx[direc];
-                y = y+dy[direc];
+    void rotate(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        for(int i=0;i<row; i++){
+            for(int j=0; j<=i;j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        return answer;
+        for(int i=0;i<row;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
