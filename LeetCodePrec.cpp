61d8ce8c15747c299a995d747f355ebf1beaeba6
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-08 21:41:01 +0900

    54. Spiral Ma54. Spiral Matrixtrix
    .

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,85 +1,34 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        // 걍 각각 검사해야하나?
-        int size = 9;
-
-        for(int i = 0 ; i < size ; i++){
-            vector<int>ch(size+1,0);
-            for(int j = 0 ; j<size ; j++){
-                if(board[i][j] != '.'){
-                    if(ch[board[i][j]-'0'] != 0){
-                        return false;
-                    }else{
-                        ch[board[i][j]-'0'] = 1;
-                    }
-                }
-            }
-        }
-        // 여기까지가 가로
-        for(int i = 0 ; i < size ; i++){
-            vector<int>ch(size+1,0);
-
-            for(int j = 0 ; j<size ; j++){
-                if(board[j][i] != '.'){
-                    if(ch[board[j][i]-'0'] != 0){
-                        return false;
-                    }else{
-                        ch[board[j][i]-'0'] = 1;
-                    }
-                }
-            }
-        }
-        //여기까지 세로
-        pair<int,int>ptr = {0,0};
-        while(1){
-            if(ptr.first > size -1 || ptr.second > size -1)break;
-            cout << "ptr f : " << ptr.first << "prt sec: " << ptr.second << "\n";
-            vector<int>ch(size+1,0);
-            for(int i = ptr.second ; i<ptr.second + 3 ; i++){
-                
-                
-                for(int j = ptr.first ; j<ptr.first + 3 ; j++){
-                    if(board[i][j] != '.'){
-                        if(ch[board[i][j]-'0'] != 0){
-                            return false;
-                        }else{
-                            ch[board[i][j]-'0'] = 1;
-                        }
-                    }                    
-                }
-            }
-            
-            if(ptr.first+3 >= size){
-                ptr.second+=3;
-                ptr.first = 0;
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        //이거 아는거임
+        int dx[] = {1,0,-1,0};
+        int dy[] = {0,1,0,-1};
+        int n = matrix.size();
+        int m = matrix[0].size() ;
+        vector<vector<int>> ch(n,vector<int>(m,0));
+        int cnt_max = n*m;
+        int direc = 0;
+        int x = 0;
+        int y = 0;
+        vector<int>answer;
+        while(answer.size() < cnt_max){
+            answer.push_back(matrix[y][x]);
+            ch[y][x] = 1;
+            int n_x = x+dx[direc];
+            int n_y = y+dy[direc];
+            if(n_x < m && 0<=n_x && n_y <n && 0<=n_y && ch[n_y][n_x]==0){
+                x = n_x;
+                y = n_y;
             }else{
-                ptr.first+=3;
+                direc++;
+                if(direc>3){
+                    direc = 0;
+                }
+                x = x+dx[direc];
+                y = y+dy[direc];
             }
-            
         }
-        return true;
-        
+        return answer;
     }
 };
-int main(){
-    vector<vector<char>>input = {
-    {'.', '.', '.', '.', '.', '.', '5', '.', '.'},
-    {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-    {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-    {'9', '3', '.', '.', '2', '.', '4', '.', '.'},
-    {'.', '.', '7', '.', '.', '.', '3', '.', '.'},
-    {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-    {'.', '.', '.', '3', '4', '.', '.', '.', '.'},
-    {'.', '.', '.', '.', '.', '3', '.', '.', '.'},
-    {'.', '.', '.', '.', '.', '5', '2', '.', '.'}
-    };
-    Solution s;
-    bool answer = s.isValidSudoku(input);
-    cout <<answer;
-    return 0;
-}
