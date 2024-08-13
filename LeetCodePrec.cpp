d6b5d5c23e7a7fcd8c254f0586d7a2751ef6f789
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-13 17:53:44 +0900

    49. Group Anagrams

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,47 +1,36 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
 
+using namespace std;
 class Solution {
 public:
-    void gameOfLife(vector<vector<int>>& board) {
-        int n = board.size();
-        int m = board[0].size();
-        vector<int>stat;
-        
-        int dx[] = {1,-1,0,0,-1,1,1,-1};
-        int dy[] = {0,0,1,-1,1,-1,1,-1};
-        for(int i = 0; i<n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                int cnt = 0;
-                for(int k = 0 ; k<8;k++){
-                    int n_i = i+dy[k];
-                    int n_j = j+dx[k];
-                    if(n_i>=0 && n_i < n && n_j >= 0 && n_j<m){
-                        if(board[n_i][n_j]==1)cnt++;
-                    }
-                }
-                stat.push_back(cnt);
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>answer;
+        map<string,int>m;
+        int cnt = 0;
+        for(int i = 0 ; i < strs.size();i++){
+            string temp = strs[i];
+            sort(temp.begin(),temp.end());
+            auto ret = m.insert({temp,cnt});
+            if(ret.second){
+                vector<string>temp_vec = {strs[i]};
+                answer.push_back(temp_vec);
+                cnt++;
+            }else{
+                answer[m[temp]].push_back(strs[i]);
             }
         }
-        int index = 0;
-        for(int i = 0; i<n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                if(board[i][j] == 0){
-                    if(stat[index] == 3)board[i][j] = 1;
-                }else{
-                    if(stat[index] <2 || stat[index]>3)board[i][j] = 0;
-                }
-                index++;
-            }
-        }        
+        return answer;
     }
 };
 
 int main(){
-    vector<vector<int>>input = {{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
+    vector<string>in = {"eat","tea","tan","ate","nat","bat"};
     Solution s;
-    s.gameOfLife(input);
+    vector<vector<string>>answer = s.groupAnagrams(in);
 
     return 0;
 }
