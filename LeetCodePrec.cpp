b04a180346d362859ee3b6c3cfcfdffa23b00949
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-14 21:28:41 +0900

    71. Simplify Path

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,36 +1,56 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <stack>
 using namespace std;
+
+
 class Solution {
 public:
-    int findMinArrowShots(vector<vector<int>>& points) {
-        int answer = 0;
-        sort(points.begin(),points.end());
-        int target;
-        for(auto temp : points){
-            if(temp == points[0]){
-                target = temp[1];
-                answer++;
-            }else{ 
-                // 안에 포함되는 경우 겹치는 경우 아예 떨어지는 경우.
-                if(target>temp[0]){
-                    target = temp[1];
-                    answer++;
-                }else{
-                    if(target>=temp[1])target = temp[1];
+    string simplifyPath(string path) {
+        string answer = "/";
+        stack<string>temp;
+        string temp_str = "";
+        for(int i = 0 ; i<path.size() ; i++){
+            if(path[i] == '/'){
+                if(temp_str!=""){
+                    temp.push(temp_str); 
+                    temp_str = "";
                 }
-
+            }else{
+                temp_str+=path[i];
             }
         }
+        if(temp_str!="")temp.push(temp_str); 
+
+        vector<string>temp_vec;
+        while(!temp.empty()){
+            string top = temp.top();
+            temp.pop();
+            if(top == ".."){
+                if(!temp.empty())temp.pop();
+            }else if(top == "."){
+                continue;
+            }
+            else{
+                temp_vec.push_back(top);
+            }
+
+        }
+        for(int i = temp_vec.size()-1; i>=0; i--){
+            answer+=temp_vec[i];
+            if(i!=0)answer+='/';
+        }
         return answer;
+
     }
 };
 
-int main(){
-    vector<vector<int>>in = {{10,16},{2,8},{1,6},{7,12}};
-    Solution s;
-    int answer = s.findMinArrowShots(in);
 
+
+int main(){
+    string in = "/a/./b/../../c/";
+    Solution s;
+    string answer = s.simplifyPath(in);
+    cout << answer;
     return 0;
 }
