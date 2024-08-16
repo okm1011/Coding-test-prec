733c3b94e762166f4b55c61971bf37f767c70c3e
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-16 19:06:34 +0900

    155. Min Stack

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,56 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-using namespace std;
-
-
-class Solution {
+class MinStack {
 public:
-    string simplifyPath(string path) {
-        string answer = "/";
-        stack<string>temp;
-        string temp_str = "";
-        for(int i = 0 ; i<path.size() ; i++){
-            if(path[i] == '/'){
-                if(temp_str!=""){
-                    temp.push(temp_str); 
-                    temp_str = "";
-                }
-            }else{
-                temp_str+=path[i];
-            }
-        }
-        if(temp_str!="")temp.push(temp_str); 
-
-        vector<string>temp_vec;
+    stack<int>st;
+    MinStack() {
+        ;
+    }
+    
+    void push(int val) {
+        st.push(val);
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        stack<int>temp = st;
+        int min = INT_MAX;
         while(!temp.empty()){
-            string top = temp.top();
+            if(temp.top()<min)min = temp.top();
             temp.pop();
-            if(top == ".."){
-                if(!temp.empty())temp.pop();
-            }else if(top == "."){
-                continue;
-            }
-            else{
-                temp_vec.push_back(top);
-            }
-
         }
-        for(int i = temp_vec.size()-1; i>=0; i--){
-            answer+=temp_vec[i];
-            if(i!=0)answer+='/';
-        }
-        return answer;
+        return min;
 
     }
 };
 
-
-
-int main(){
-    string in = "/a/./b/../../c/";
-    Solution s;
-    string answer = s.simplifyPath(in);
-    cout << answer;
-    return 0;
-}
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
