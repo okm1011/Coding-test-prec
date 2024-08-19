f9452c860212d81293eb1ffafccb4a41a7fffa38
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-19 18:46:13 +0900

    .

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,31 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
+    int calculate(string s) {
+        //4가지 경우 +- or - or 숫자 or () or ' '
+        // )) 부터 스택에 담으면 될텐데
         stack<int>st;
-
-        for(int i = 0 ; i < tokens.size() ; i++){
-            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
-                int first = st.top();
-                st.pop();
-                int second = st.top();
-                st.pop();
-                int temp;
-                if(tokens[i] == "+"){
-                    temp = first+second;
-                }else if(tokens[i] == "-"){
-                    temp = second-first;
-                }else if(tokens[i] == "*"){
-                    temp = first*second;
-                }else{
-                    temp = second/first;
-                }
-                cout << "temp = " << temp <<"\n";
-                st.push(temp);
-            }else{
-                int temp = stoi(tokens[i]);
-                st.push(temp);
+        vector<pair<int,int>>ptr;
+        int left = 0;
+        int right = s.size()-1;
+        vector<int>left_vec;
+        vector<int>right_vec;
+        while(left<right){
+            if(s[left] == '('){
+                left_vec.push_back(left);
             }
+            if(s[right] == ')'){
+                right_vec.push_back(right);
+            }
+            left++;
+            right--;
         }
-        return st.top();
+        for(int i = 0 ; i < left_vec.size() ; i++){
+            ptr.push_back({left_vec[i],right_vec[i]});
+        }
+        pair<int,int>last = {INT_MAX,-INT_MAX};
+        for(int i = ptr.size()-1 ; i>=0 ; i--){
+            for(int j = ptr[i].second-1; j> ptr[i].first ; j++){
+                if(s[j] != ' ' && j>last.second && j<last.first){
+                    st.push(s[j]);
+                }
+            }
+            last = ptr[i];
+        }
+        
+        return 0;
     }
 };
+
+int main(){
+    string input = "(1+(4+5+2)-3)+(6+8)";
+    Solution s;
+    int answer =s.calculate(input);
+
+    return 0;
+}
