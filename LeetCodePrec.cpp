f3df6073bb9171f903bfee10f9a37446063a10b1
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-21 21:48:45 +0900

    138. Copy List with Random Pointer

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,25 +1,66 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy=new ListNode();
-        ListNode* temp=dummy;
-        int carry=0;
-        while(l1!=NULL || l2!=NULL || carry){
-            int sum=0;
-            if(l1!=NULL){
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            sum+=carry;
-            carry=sum/10;
-            ListNode* newnode=new ListNode(sum%10);
-            temp->next=newnode;
-            temp=temp->next;
+    Node* copyRandomList(Node* head) {
+        if(head == NULL)return NULL;
+        Node* copy_head = new Node(head->val);
+        Node* dummy_temp = copy_head;
+        Node* temp = head;
+        map<Node* , int> m_ch;
+        int cnt = 0;
+        m_ch.insert({temp,cnt});
+        // head check
+
+        while(temp->next != NULL){
+            Node* next_node = new Node(temp->next->val);
+            cnt++;
+            m_ch.insert({temp->next,cnt});
+            dummy_temp->next = next_node;
+            dummy_temp = next_node;
+            temp = temp->next;
         }
-        return dummy->next;
+        dummy_temp->next = NULL;
+
+
+        
+        int turn = 0;
+        dummy_temp = copy_head;
+        temp = head;
+        while(turn <= cnt){
+            if(temp->random != NULL){
+                int index = m_ch[temp->random];
+                Node* target = copy_head;
+                for(int i = 0 ; i<index ; i++){
+                    target = target->next;
+                }
+                dummy_temp->random = target;
+            }else{
+                dummy_temp->random = NULL;
+            }
+            temp = temp->next;
+            dummy_temp = dummy_temp -> next;
+            turn++;
+        }
+
+
+
+
+
+        return copy_head;
     }
 };
