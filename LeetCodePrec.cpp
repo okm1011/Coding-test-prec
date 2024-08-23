78a5d894a4c81ac3618cc2bbe6bd3d59f8a8805b
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-23 19:58:30 +0900

    19. Remove Nth Node From End of List

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -10,66 +10,43 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
         stack<ListNode*>st;
         ListNode*temp = head;
-        int index = 1;
-        ListNode* first = nullptr;
-        ListNode* end = nullptr;
-        
-        while(1){
+        while(temp->next != nullptr){
+            st.push(temp);
+            temp= temp -> next;
+        }
+        st.push(temp);
+        if(st.size() == 1)return nullptr;
 
-            if(index >= left && index <= right){
-                st.push(temp);
-            }
-            if(index == left-1 && left - 1 >=1){
-                first = temp;
-            }
-            if(index == right+1){
-                end = temp;
-                break;
-            }
-            if(temp->next != nullptr){
-                temp = temp->next;
-                index++;
-            }else{
-                break;
-            }
-        }
-        if(first == nullptr){
-            head = st.top();
-            st.pop();
-            temp = head;
+        int cnt = 0;
+        ListNode*prev = NULL;
+        ListNode*cur = NULL;
+        while(cnt < n && !st.empty()){
             
-            while(!st.empty()){
-                temp->next = st.top();
+            if(cur == NULL){
+                cur = st.top();
                 st.pop();
-                temp = temp->next;
-            }
-            temp->next = end;
-        }else{
-            index = 1;
-            temp = head;
-            while(1){
-                if(index == left-1){
-                    temp->next = first;
-                    index++;
-                }else if(index == left){
-                    while(!st.empty()){
-                        temp->next = st.top();
-                        st.pop();
-                        temp = temp->next;
-                    }
-                    temp->next = end;
-                       
-                    break;                 
-                }else{
-                    temp = temp->next;
-                    index++;
-                }
+                cnt++;
+            }else{
+                prev = cur;
+                cur = st.top();
+                st.pop();
+                cnt++;
             }
         }
+        if(st.empty()){
+            return prev;
+        }else{
+            temp = st.top();
+            
+            temp -> next = prev;
+            return head;
+        }
+
         
-        return head;
+
     }
 };
