310e65527759913bbe1aa898df38918ff09c4147
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-26 22:28:56 +0900

    .
    .

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,50 +1,22 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
+ * Definition for a binary tree node.
+ * struct TreeNode {
  *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if(head == nullptr)return nullptr;
-        set<int>ch;
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
-        ListNode* check = head;
-        ListNode* last_check = dummy;
-
-        while(1){
-
-            if(check->next == nullptr)break;
-
-            if(check->val == check->next->val){
-                int same_val = check->val;
-                while(1){
-                    if(check->next != nullptr)check = check->next;
-                    else {
-                        temp -> next = nullptr;
-                        return dummy->next;
-                    }
-
-                    if(same_val != check->val)break;
-                }
-            }else{
-                
-                last_check = check;
-                temp->next = last_check;
-                temp = temp->next;
-                if(check->next != nullptr)check = check->next;
-                else break;
-            }
-
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        TreeNode* temp = new TreeNode();
+        for(int i = 0 ; i<preorder.size();i++){
+            preorder[i];
+            inorder[i];
         }
-        
-        if(last_check->val != check->val) temp -> next = check;
-        return dummy->next;
+
     }
 };
