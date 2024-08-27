9adfe74fc93bf7c26333c0fd820129a101af431a
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-27 21:05:09 +0900

    105

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -11,12 +11,26 @@
  */
 class Solution {
 public:
+    int index = 0;
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        TreeNode* temp = new TreeNode();
-        for(int i = 0 ; i<preorder.size();i++){
-            preorder[i];
-            inorder[i];
-        }
+        if(preorder.empty())return nullptr;
+        TreeNode* answer = make_node(preorder,inorder,0,preorder.size()-1);
+        return answer;
 
+        
+    }
+    TreeNode* make_node(vector<int>& preorder, vector<int>& inorder,int start, int end){
+        if(start>end)return nullptr;
+        TreeNode* temp = new TreeNode(preorder[index]);
+        int i;
+        for(i = start ; i<=end ; i++){
+            if(preorder[index] == inorder[i]){
+                index++;
+                break;
+            }
+        }
+        temp->left = make_node(preorder,inorder,start,i-1);
+        temp->right = make_node(preorder,inorder,i+1,end);
+        return temp;
     }
 };
