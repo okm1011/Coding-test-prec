eb81215c7189b8e32021dc837f70e633110d2f9e
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-08-29 23:36:40 +0900

    .

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -9,42 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     int index;
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        if(inorder.empty())return nullptr;
+        if(postorder.empty())return nullptr;
+        if(postorder.size() == 1){
+            TreeNode* answer = new TreeNode(postorder[0]);
+            return answer;
+        }
         index = postorder.size()-1;
-        TreeNode* answer = new TreeNode(postorder[postorder.size()-1]);
-        int i;
-        for(i = 0 ; i<postorder.size();i++){
-            if(inorder[i] == postorder[postorder.size()-1]){
-                break;
-            }
-        }
-        answer->left = make_node(inorder,postorder,{0,i-1});
-        answer->right = make_node(inorder,postorder, {i+1,postorder.size()-1});
+        TreeNode* answer = make_node(inorder,postorder,0,postorder.size()-1);
         return answer;
+
+        
     }
-    TreeNode* make_node(vector<int>& inorder, vector<int>& postorder , pair<int,int>pos){
-        if(pos.first>pos.second)return nullptr;
-        int max_pos = INT_MIN;
-        for(int i = pos.first ; i<=pos.second ; i++){
-            int target = inorder[i];
-            auto pos_idx = find(postorder.begin(),postorder.end(),target);
-            if(pos_idx-postorder.begin() > max_pos)max_pos =pos_idx-postorder.begin();
-        }
-        TreeNode* temp = new TreeNode(postorder[max_pos]);
+    TreeNode* make_node(vector<int>& inorder, vector<int>& postorder,int start, int end){
+        if(start > end)return nullptr;
+
+        TreeNode* temp = new TreeNode(postorder[index]);
 
         int i;
-        for(i = pos.first ; i<=pos.second;i++){
-            if(inorder[i] == postorder[max_pos]){
+        for(i = start ; i<end ; i++ ){
+            if(inorder[i] == postorder[index]){
+                index--;
                 break;
             }
-        }        
-        temp->left = make_node(inorder,postorder,{pos.first,i-1});
-        temp->right = make_node(inorder,postorder,{i+1,pos.second});
-
+        }
+        temp->right = make_node(inorder,postorder,i+1,end);
+        temp->left = make_node(inorder,postorder,start,i-1);
         return temp;
     }
 };
