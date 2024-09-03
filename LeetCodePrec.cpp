19f40096d85419f72fb273c7790512356e60bd54
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-09-04 00:09:03 +0900

    117. Populating Next Right Pointers in Each Node II

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -17,31 +17,48 @@ public:
 */
 
 class Solution {
-public:
+public: 
+    static bool comp(pair<Node*,int>a ,pair<Node*,int>b ){
+        return a.second < b.second;
+        
+    }
+
+    vector<pair<Node*,int>>pair;
     Node* connect(Node* root) {
         if(root==nullptr) return nullptr;
-        Node* temp = root;
-        vector<Node*>arr;
-        while(1){
-            arr.push_back(temp);
-            if(temp->left != NULL)temp = temp->left;
-            else break;
-        }
-        int cnt = arr.size();
-        while(cnt>0){
-            int round = 0;
-            temp = arr[cnt-1];
-            for(int i = 1 ; i<cnt ; i++){
-                
-
-
-                temp = temp -> next;
-                round++;
+        pair.push_back({root,1});
+        add_node(root,2);
+        sort(pair.begin(),pair.end(),comp);
+        int val = 1;
+        Node*temp = root;
+        for(int i = 1 ; i<pair.size();i++){
+            cout << "val : " << pair[i].first->val << " num : "<<pair[i].second << "\n";
+            if(pair[i].second == val){
+                temp->next = pair[i].first;
+                temp = pair[i].first;
+            }else{
+                temp->next = NULL;
+                temp = pair[i].first;
             }
-
+            val = pair[i].second;
         }
-
         return root;
+    }
+    void add_node(Node* temp,int num){
+        Node* target = temp;
+        vector<Node*>cont;
+        if(target->left != NULL){
+            pair.push_back({target->left , num});
+            cont.push_back(target->left);
+            add_node(target->left , num+1);
+        }
+        if(target->right != NULL){
+            pair.push_back({target->right,num});
+            cont.push_back(target->right);
+            add_node(target->right , num+1);
+        }
+        
+        
     }
 
 
