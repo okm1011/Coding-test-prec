245d32dbc4770654e8a871bfe461640c8d502b6e
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-09-12 00:37:57 +0900

    129.Sum Root to Leaf Numbers

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -11,32 +11,39 @@
  */
 class Solution {
 public:
-    vector<TreeNode*>arr;
-    void flatten(TreeNode* root) {
-        // 포인트는 어떻게 in - place에서 교체를 하느냐인데....
-        // pre order 순차대로가면서 right바꿔버리면 .... 참조가안되잖아
-        // 일단 순회부분부터 만들어보자 메모리 신경안쓰고
-        if(root == NULL)return;
-        pre_order(root);
-        TreeNode* temp = root;
-        for(int i = 1 ; i<arr.size();i++){
-            temp->left = NULL;
-            temp->right = arr[i];
-            temp = arr[i];
-        }
-        return;
+    bool selected = false;
+    int answer=0;
+    int sumNumbers(TreeNode* root) {
+        vector<TreeNode*>arr;
+        search(root,arr);
+        return answer;
     }
-    void pre_order(TreeNode* temp){
-        TreeNode* target = temp;
-        arr.push_back(target);
-        if(temp->left != NULL){
-            target = temp->left;
-            pre_order(target);
-        }
-        if(temp->right != NULL){
-            target = temp->right;
-            pre_order(target);
-        }
+    void search(TreeNode* target , vector<TreeNode*>arr){
 
-    } 
+        arr.push_back(target);
+        if(target->left == NULL){
+            int sum = 0;
+            for(int i = 0 ; i<arr.size();i++){
+                sum  = sum*10 + arr[i]->val;
+            }
+            answer+=sum;
+            cout << "left :" << answer<<"\n";
+            return;
+        }else{
+            search(target->left,arr);
+        }
+        
+        if(target->right == NULL){
+            int sum = 0;
+            for(int i = 0 ; i<arr.size();i++){
+                sum  = sum*10 + arr[i]->val;
+            }
+            answer+=sum;
+            cout << "right :" << answer<<"\n";
+            return;
+        }else{
+            search(target->right,arr);
+        }
+        
+    }
 };
