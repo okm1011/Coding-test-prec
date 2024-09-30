3bc079d0e96a1d4a06bcff2cb03a44df5502dcd6
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-09-30 22:07:55 +0900

    다익스트라

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -6,34 +6,41 @@ using namespace std;
 
 int solution(int N, vector<vector<int> > road, int K) {
     int answer = 0;
-
-    // bfs도 애매 dfs도 애매함 이럴때는 dp로 푸는거지
-    vector<vector<int>>arr (N+1,vector<int>(N+1,1000000));
-    for(int i = 1 ; i<=N;i++){
-        for(int j = 1; j<=N;j++){
-            if(i==j)arr[i][j] = 0;              
-        }
-    }
-    // i 에서 j로 가는데 자기 자신이면 값 0임.
+    vector<vector<int>>arr(N+1,vector<int>(N+1,10000000));
     for(int i = 0 ; i<road.size() ; i++){
         arr[road[i][0]][road[i][1]] = min(arr[road[i][0]][road[i][1]],road[i][2]);
         arr[road[i][1]][road[i][0]] = min(arr[road[i][1]][road[i][0]],road[i][2]);
     }
-    // 처음 주어진 애들로 초기화 >> 한번에 가는경우
-    
-    //이제 3중 포문으로 플로이드 와샬 하는거임 각 중간노드 k 일때 i에서 j 가는거 갱신하는거지 >> DP지
-    // 근데 이게 중간 노드가 여러개면 안되는거아닌가??
-    for(int k = 1; k<=N ; k++){
-        for(int i = 1; i<=N ; i++){
-            for(int j = 1; j<=N;j++){
-                arr[i][j] = min(arr[i][j],arr[i][k]+arr[k][j]);
+    for(int i = 1 ; i<=N ; i++){
+        for(int j = 1 ; j<=N;j++){
+            if(i==j)arr[i][j] = 0;
+
+        }
+    }
+    // 이렇게 초기화 시켜놓음 i 에서 j 까지의 cost
+    priority_queue<pair<int,int>>pq;
+    //여기서 현재까지 cost,다음 목표 이렇게 갈꺼임 대신 cost 내림정렬이니까 -로 넣어줘야함.
+    vector<int>dist(N+1,10000000);
+    pq.push({0,1});
+
+    while(!pq.empty()){
+        pair<int,int>temp = pq.top();
+        pq.pop();
+        int cur = temp.second;
+        int cost = -temp.first;
+        for(int i = 1; i<=N; i++){
+            if(arr[cur][i] != 10000000){
+                if(dist[i] > cost + arr[cur][i]){
+                    dist[i] = cost + arr[cur][i];
+                    pq.push({-(cost + arr[cur][i]),i});
+                }
+
             }
         }
     }
-
-    for(int i = 1 ; i<=N;i++ ){
-        if(arr[1][i] <= K)answer++;
+    for(int i = 1; i<=N;i++){
+        cout << "i: " << i << "dist: "<<dist[i] << "\n";
+        if(dist[i]<=K)answer++;
     }
-
     return answer;
 }
