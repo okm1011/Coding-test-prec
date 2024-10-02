25aef8620f2ba05d766cf2c03ae27c09f00b4d96
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-10-02 19:28:11 +0900

    행렬 테두리 회전하기

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,46 +1,89 @@
-#include <iostream>
+#include <string>
 #include <vector>
-#include <queue>
-#include <climits>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int solution(int N, vector<vector<int> > road, int K) {
-    int answer = 0;
-    vector<vector<int>>arr(N+1,vector<int>(N+1,10000000));
-    for(int i = 0 ; i<road.size() ; i++){
-        arr[road[i][0]][road[i][1]] = min(arr[road[i][0]][road[i][1]],road[i][2]);
-        arr[road[i][1]][road[i][0]] = min(arr[road[i][1]][road[i][0]],road[i][2]);
-    }
-    for(int i = 1 ; i<=N ; i++){
-        for(int j = 1 ; j<=N;j++){
-            if(i==j)arr[i][j] = 0;
-
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+    vector<int> answer;
+    // 와 ㅅㅂ 이거 헷갈리겠네 가로세로
+    // rows 는 y의 최대값 = i = 앞의 수 // columns는 x의 최대값 = j = 뒤의 수
+    vector<vector<int>>m(rows+1,vector<int>(columns+1,0));
+    int cnt = 1;
+    for(int i = 1; i<=rows ; i++){
+        for(int j = 1; j<=columns;j++){
+            m[i][j] = cnt;
+            cnt++;
         }
     }
-    // 이렇게 초기화 시켜놓음 i 에서 j 까지의 cost
-    priority_queue<pair<int,int>>pq;
-    //여기서 현재까지 cost,다음 목표 이렇게 갈꺼임 대신 cost 내림정렬이니까 -로 넣어줘야함.
-    vector<int>dist(N+1,10000000);
-    pq.push({0,1});
 
-    while(!pq.empty()){
-        pair<int,int>temp = pq.top();
-        pq.pop();
-        int cur = temp.second;
-        int cost = -temp.first;
-        for(int i = 1; i<=N; i++){
-            if(arr[cur][i] != 10000000){
-                if(dist[i] > cost + arr[cur][i]){
-                    dist[i] = cost + arr[cur][i];
-                    pq.push({-(cost + arr[cur][i]),i});
-                }
-
-            }
+    
+    for(int i =0 ; i<queries.size();i++){
+        pair<int,int>st = {queries[i][0],queries[i][1]};
+        pair<int,int>ed = {queries[i][2],queries[i][3]};
+        //여기서 그냥 4번해버려?
+        vector<int>temp;
+        int row_pt = st.first;
+        int col_pt = st.second;
+        
+        while(col_pt < ed.second){
+            temp.push_back(m[row_pt][col_pt]);
+            col_pt++;
         }
+        while(row_pt < ed.first){
+            temp.push_back(m[row_pt][col_pt]);
+            row_pt++;
+        }
+        while(col_pt > st.second){
+            temp.push_back(m[row_pt][col_pt]);
+            col_pt--;
+        }   
+        while(row_pt > st.first){
+            temp.push_back(m[row_pt][col_pt]);
+            row_pt--;
+        }
+        int insert = temp.size()-1;
+        row_pt = st.first;
+        col_pt = st.second;
+        
+        while(col_pt < ed.second){
+            m[row_pt][col_pt] = temp[insert];
+            insert++;
+            if(insert == temp.size())insert=0;
+            col_pt++;
+        }
+        while(row_pt < ed.first){
+            m[row_pt][col_pt] = temp[insert];
+            insert++;
+            if(insert == temp.size())insert=0;
+            row_pt++;
+        }
+        while(col_pt > st.second){
+            m[row_pt][col_pt] = temp[insert];
+            insert++;
+            if(insert == temp.size())insert=0;
+            col_pt--;
+        }   
+        while(row_pt > st.first){
+            m[row_pt][col_pt] = temp[insert];
+            insert++;
+            if(insert == temp.size())insert=0;
+            row_pt--;
+        }
+        sort(temp.begin(),temp.end());
+        answer.push_back(temp[0]);
     }
-    for(int i = 1; i<=N;i++){
-        cout << "i: " << i << "dist: "<<dist[i] << "\n";
-        if(dist[i]<=K)answer++;
-    }
+    
+    
     return answer;
+}
+
+int main(){
+
+    int rows = 6;
+    int columns = 6;
+    vector<vector<int>> queries = {{2,2,5,4},{3,3,6,6,},{5,1,6,3}};
+    vector<int>answer= solution(rows,columns,queries);
+
+    return 0;
 }
