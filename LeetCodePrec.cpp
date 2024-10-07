62bd2edcc9258709a7ef47980df5844a101f6ec4
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-10-07 22:19:41 +0900

    조이스틱 - 개고생 품

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,19 +1,10 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
-int dfs(string name, int current , int cnt){
-    if(cnt >= name.size()-1)return 3;
 
-    if(name[current+cnt] != 'A' && name[name.size()-1-(cnt-1)] == 'A'){
-        return 1;
-    }else if(name[name.size()-1-(cnt-1)] != 'A' && name[current+cnt] == 'A' ){
-        return 2;
-    }else{
-        return  dfs(name,current,cnt+1);
-    }
-}
 int solution(string name) {
     int answer = 0;
     int all_cnt = 0;
@@ -21,49 +12,51 @@ int solution(string name) {
         if(name[i] !='A')all_cnt+=min((name[i]-'A'),('Z'-name[i]+1));
     }
     answer+=all_cnt;
-    int index = 0;
-    int direc = 0;
-    int move_cnt = 0;
-    while(all_cnt != 0){
-        if(index == -1)index = name.size()-1;
-        if(name[index] != 'A'){
-            all_cnt-=min((name[index]-'A'),('Z'-name[index]+1));
-            if(all_cnt == 0)break;
-            direc = dfs(name,0,1);
-            cout << "index: " << index << "direc: "<<direc <<"\n";
-            if(direc == 1){
-                index++;
-                move_cnt++;
-            }else if(direc == 2){
-                index--;
-                move_cnt++;
-            }else{
-                index++;
-                move_cnt++;
-            }       
-        }else{
-            if(direc == 1){
-                index++;
-                move_cnt++;
-            }else if(direc == 2){
-                index--;
-                move_cnt++;
-            }else{
-                index++;
-                move_cnt++;
-            }            
-        }
-        
-        // 1일때 직진 2일때 후진 3일때 노상관
+    
+    vector<int>steps;
+    // 오,왼,오-왼,왼-오
+    // 오
+    int index = name.size()-1;
+    int right_step = name.size()-1;
+    while(name[index] == 'A' && index > 0){
+        right_step --;
+        index--;
     }
-    answer += move_cnt;
+    index = 1;
+    int left_step = name.size()-1;
+    while(name[index] == 'A' && index <= name.size()-1){
+        left_step --;
+        index++;
+    }
+    steps.push_back(right_step);
+    steps.push_back(left_step);
+    
+    for(int i = 0 ; i <name.size();i++){
+        int start = 0;
+        int end = 0;
+       //여기서 왼-오 , 오-왼 스타트 엔드가 애매하니까 싹다해봐그냥 어차피 name 20자임
+        if(name[i] != 'A'){
+            start = i;
+            int index = i+1;
+            while(1){
+                if(name[index] == 'A'){
+                    index++;   
+                }else{
+                    break;
+                }
+                
+            }
+            if(index < name.size())end = index;
+            steps.push_back(min(start + start + name.size()-end , name.size()-end +name.size()-end + start ));
+        }
+    }
+    sort(steps.begin(),steps.end());
+
+    answer+=steps[0];
+    
+        // BBBAAABBAAAAAAAAAAAAAAAAAAAAAAABBBAABB
+        
+        
+        
     return answer;
-}
-
-int main(){
-
-    string name = "BBBBAAAABA";
-    int res = solution(name);
-    cout << res;
-    return 0;
 }
