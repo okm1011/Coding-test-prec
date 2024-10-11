557d4e24eee0aca6ab79b9f75fd8e891495ff158
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-10-11 18:32:53 +0900

    Update LeetCodePrec.cpp

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,45 +1,31 @@
 #include <iostream>
 #include<vector>
 using namespace std;
-int cnt_same(vector<int>same){
-    int temp = 0 ;
-    int max = 0;
-    for(int i = 0 ; i < same.size(); i++){
-        if(same[i] == 1){
-            temp++;
-            if(temp>max) max = temp;
-        }else{
-            temp = 0;
+
+int find_sq(vector<vector<int>> board, int y , int x , int max_y,int max_x, int step){
+    if(y+step >= max_y || x+step >= max_x)return step;
+
+    for(int i = y ; i<y+step+1;i++){
+        for(int j = x ; j<x+step+1 ; j++){
+            if(board[i][j] == 0)return step;
         }
     }
-    return max;
+    return find_sq(board,y,x,max_y,max_x,step+1);
 }
+
 int solution(vector<vector<int>> board)
 {
-    int answer = 1;
+    int answer = 0;
     int y = board.size();
     int x = board[0].size();
-    if(x == 0 || y == 0)return 0;
-    vector<int>same (x,0);
-    for(int i = 0 ; i < x ; i++){
-        same[i] = board[0][i];
-    }
-    for(int i = 1 ; i<y;i++){
-        int h = i+1;
-
-        for(int j = 0 ; j<x ; j++){            
-            if(same[j] == 1 && board[i][j] == 1){
-                same[j] = 1;
-            } 
-            else{
-                same[j] = 0;
-            } 
+    if(x==0 || y==0)return 0;
+    for(int i = 0 ; i<y;i++){
+        for(int j = 0 ; j<x ; j++){
+            if(board[i][j] == 1){
+                int temp = find_sq(board,i,j,y,x,1);
+                if(temp*temp > answer)answer = temp*temp;
+            }
         }
-        int cnt = cnt_same(same);
-        int temp_answer = 0;
-        if(cnt >= h) temp_answer = h*h;
-        if(temp_answer>answer)answer = temp_answer;
-
     }
     
     return answer;
