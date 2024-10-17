cd177ef375b92d07d243c3bba9e357f2319779e4
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-10-18 00:35:02 +0900

    1018

--- LeetCodePrec.cpp
+++ LeetCodePrec.cpp
@@ -1,61 +1,56 @@
-#include <string>
+
+#include <iostream>
 #include <vector>
-#include <stack>
-
 using namespace std;
-vector<vector<int>>min_answer;
-
-void dfs(vector<stack<int>>towers,vector<vector<int>>steps,int n ){
-    if(towers[0].size() == n){
-        if(steps.size()>min_answer.size())min_answer = steps;
+vector<int> solution(int n) {
+    vector<int> answer;
+    if(n==1){
+        answer.push_back(1);
+        return answer;
     }
-    for(int i = 0 ; i<3;i++){
-        if(!towers[i].empty()){
-            int right = i+1;
-            int left = i-1;
-            if(right > 2) right = 0;
-            if(left<0) left = 2;
-            if(towers[right].empty() || towers[right].top()>towers[i].top() ){
-                int top = towers[i].top();
-                towers[i].pop();
-                towers[right].push(top);
-                steps.push_back({i,right});
-                dfs(towers,steps,n);
-            }
-            if( towers[left].empty() || towers[left].top()>towers[i].top()){
-                int top = towers[i].top();
-                towers[i].pop();
-                towers[left].push(top);
-                steps.push_back({i,left});
-                dfs(towers,steps,n);                
-            }
-            
+    //? 이거 쉬운거아닌가
+    vector<vector<int>>arr;
+    int sum = 0;
+    int cnt = 0;
+    while(cnt < n){
+        cnt++;
+        sum+=cnt;
+        vector<int>temp(cnt,0);
+        arr.push_back(temp);
+    }
+    // arr만들기 이제 어떻게 채워야할지 생각해봐야함.
+    int x = 0 ;
+    int y = 0 ;
+    int num = 1;
+    int direc = 0;
+    int dx[] = {0,1,-1};
+    int dy[] = {1,0,-1};
+
+
+    while(num<=sum){
+        arr[y][x] = num;
+        num++;
+        int temp_x = x + dx[direc];
+        int temp_y = y + dy[direc];
+        //cout << "x: " << temp_x << " y: "<<temp_y << "\n";
+
+        if(temp_y<0 || temp_y>=arr.size() || temp_x<0 || temp_x >= arr[y].size() || arr[temp_y][temp_x] != 0 ){
+            direc = (direc + 1) % 3; 
+            temp_x = x + dx[direc]; 
+            temp_y = y + dy[direc];  
+        }
+        x = x+dx[direc];
+        y = y+dy[direc];
+        
+    }
+    
+    
+    for(int i = 0 ; i<arr.size();i++){
+        for(int j = 0 ; j < arr[i].size();j++){
+            answer.push_back(arr[i][j]);
         }
     }
-}
 
-vector<vector<int>> solution(int n) {
-    vector<vector<int>> answer;
-    // 음............... 그냥 중복 가능 모든 경우의수 다 때려박으면 될 듯.
-    vector<vector<int>> temp;
-    vector<stack<int>>towers;
-    stack<int>tower1;
-    for(int i = n ; i >= 1 ; i--){
-        tower1.push(i);
-    }
-    stack<int>tower2;
-    stack<int>tower3;
-    towers.push_back(tower1);
-    towers.push_back(tower2);
-    towers.push_back(tower3);
-    dfs(towers,temp,n);
-    answer = min_answer;
+    
     return answer;
-}
-int main(){
-    int n = 2;
-    vector<vector<int>>answer =solution(n);
-
-
-
 }
